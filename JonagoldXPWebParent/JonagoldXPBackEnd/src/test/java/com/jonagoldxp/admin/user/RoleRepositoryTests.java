2e343a49469184faeccbcaff41e33f648aab409e
mkozlo12 <01169195@pw.edu.pl>
2022-12-05 00:58:45 +0100

    Added test to insert new necessary roles for JonagoldXP web application.

--- JonagoldXPWebParent/JonagoldXPBackEnd/src/test/java/com/jonagoldxp/admin/user/RoleRepositoryTests.java
+++ JonagoldXPWebParent/JonagoldXPBackEnd/src/test/java/com/jonagoldxp/admin/user/RoleRepositoryTests.java
@@ -1,6 +1,7 @@
 package com.jonagoldxp.admin.user;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.InstanceOfAssertFactories.list;
 
 import com.jonagoldxp.common.entity.Role;
 import org.junit.jupiter.api.Test;
@@ -9,6 +10,8 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.annotation.Rollback;
+
+import java.util.List;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = Replace.NONE) // to test a real (and not in-memory) database
@@ -25,4 +28,16 @@ public class RoleRepositoryTests {
 
     }
 
+    @Test
+    public void testCreateNecessaryRoles(){
+        Role roleSalesperson = new Role("Salesperson", "manage products' prices & shipment, customers' orders.");
+
+        Role roleEditor = new Role("Editor", "manage categories, brands, products, articles and menus.");
+
+        Role roleShipper = new Role("Shipper", "view orders & products, update orders' statuses.");
+
+        Role roleAssistant = new Role("Assistant", "manage questions and reviews");
+        repo.saveAll(List.of(roleSalesperson, roleEditor, roleShipper, roleAssistant));
+
+    }
 }
