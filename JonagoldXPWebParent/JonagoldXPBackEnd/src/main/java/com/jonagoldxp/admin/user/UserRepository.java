2aeeef6b83b57b38f8b23755aa61657046945657
mkozlo12 <01169195@pw.edu.pl>
2022-12-07 19:41:44 +0100

    Fixed bug - added annotation to UserRepository repository.

--- JonagoldXPWebParent/JonagoldXPBackEnd/src/main/java/com/jonagoldxp/admin/user/UserRepository.java
+++ JonagoldXPWebParent/JonagoldXPBackEnd/src/main/java/com/jonagoldxp/admin/user/UserRepository.java
@@ -2,6 +2,8 @@ package com.jonagoldxp.admin.user;
 
 import com.jonagoldxp.common.entity.User;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends CrudRepository<User, Integer> {
 }
