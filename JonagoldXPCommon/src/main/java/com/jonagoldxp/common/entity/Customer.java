05578c9efffb59d2c810cd747e611227a3d9fda9
mkozlo12 <01169195@pw.edu.pl>
2023-01-07 15:08:34 +0100

    Implemented minor changes and added EntityScan import to JonagoldXPFrontEndApplication

--- JonagoldXPCommon/src/main/java/com/jonagoldxp/common/entity/Customer.java
+++ JonagoldXPCommon/src/main/java/com/jonagoldxp/common/entity/Customer.java
@@ -5,7 +5,7 @@ import jakarta.persistence.*;
 import java.util.Date;
 
 @Entity
-@Table(name = "customers")
+@Table (name = "customers")
 public class Customer {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -54,30 +54,35 @@ public class Customer {
 //    private Country country;
 
     public Customer(){
-
     }
 
     public Integer getId() {
+
         return id;
     }
 
     public void setId(Integer id) {
+
         this.id = id;
     }
 
     public String getEmail() {
+
         return email;
     }
 
     public void setEmail(String email) {
+
         this.email = email;
     }
 
     public String getPassword() {
+
         return password;
     }
 
     public void setPassword(String password) {
+
         this.password = password;
     }
 
@@ -86,78 +91,97 @@ public class Customer {
     }
 
     public void setFirstName(String firstName) {
+
         this.firstName = firstName;
     }
 
     public String getLastName() {
+
         return lastName;
     }
 
     public void setLastName(String lastName) {
+
         this.lastName = lastName;
     }
 
     public String getPhoneNumber() {
+
         return phoneNumber;
     }
 
     public void setPhoneNumber(String phoneNumber) {
+
         this.phoneNumber = phoneNumber;
     }
 
     public String getAddressLine1() {
+
         return addressLine1;
     }
 
     public void setAddressLine1(String addressLine1) {
+
         this.addressLine1 = addressLine1;
     }
 
     public String getAddressLine2() {
+
         return addressLine2;
     }
 
     public void setAddressLine2(String addressLine2) {
+
         this.addressLine2 = addressLine2;
     }
 
     public String getCity() {
+
         return city;
     }
 
     public void setCity(String city) {
+
         this.city = city;
     }
 
     public String getPostalCode() {
+
         return postalCode;
     }
 
     public void setPostalCode(String postalCode) {
+
         this.postalCode = postalCode;
     }
 
     public String getVerificationCode() {
+
         return verificationCode;
     }
 
     public void setVerificationCode(String verificationCode) {
+
         this.verificationCode = verificationCode;
     }
 
     public boolean isEnabled() {
+
         return enabled;
     }
 
     public void setEnabled(boolean enabled) {
+
         this.enabled = enabled;
     }
 
     public Date getCreatedTime() {
+
         return createdTime;
     }
 
     public void setCreatedTime(Date createdTime) {
+
         this.createdTime = createdTime;
     }
 
