68725489e2191380985b2d796300aea020ec8906
atorz <01169253@pw.edu.pl>
2023-01-10 23:29:50 +0100

    Added Category saving function

--- JonagoldXPWebParent/JonagoldXPBackEnd/src/main/java/com/jonagoldxp/admin/category/CategoryController.java
+++ JonagoldXPWebParent/JonagoldXPBackEnd/src/main/java/com/jonagoldxp/admin/category/CategoryController.java
@@ -5,6 +5,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.util.StringUtils;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.io.IOException;
 import java.util.List;
 
 @Controller
@@ -28,5 +35,21 @@ public class CategoryController {
         model.addAttribute("pageTitle", "Create New Category");
 
         return "categories/category_form";
+    }
+
+    @PostMapping("/categories/save")
+    public String saveCategory(Category category, @RequestParam("fileImage")MultipartFile multipartFile, RedirectAttributes ra){
+        if (!multipartFile.isEmpty()) {
+            String fileName = StringUtils.cleanPath(multipartFile.getOriginalFilename());
+            category.setImage(fileName);
+
+            // TODO programme image upload
+            Category savedCategory = service.save(category);
+        } else {
+            service.save(category);
+        }
+
+        ra.addFlashAttribute("message", "The category has been saved successfully.");
+        return "redirect:/categories";
     }
 }
