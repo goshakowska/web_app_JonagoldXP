a418c47310214d855162557baca8214e605e90c0
mkozlo12 <01169195@pw.edu.pl>
2023-01-10 13:52:43 +0100

    Added new queries into UserRepository interface.

--- JonagoldXPWebParent/JonagoldXPBackEnd/src/main/java/com/jonagoldxp/admin/user/UserRepository.java
+++ JonagoldXPWebParent/JonagoldXPBackEnd/src/main/java/com/jonagoldxp/admin/user/UserRepository.java
@@ -1,9 +1,18 @@
 package com.jonagoldxp.admin.user;
 
 import com.jonagoldxp.common.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends CrudRepository<User, Integer> {
+    @Query("SELECT u FROM User u WHERE u.email = :email")
+    public User getUserByEmail(@Param("email")String email);
+
+    @Query("SELECT u FROM User u WHERE concat(u.id, ' ', u.email, ' ', u.firstName, ' ', u.lastName) LIKE%?1%")
+    public Page<User> findAll(String keyword, Pageable pageable);
 }
