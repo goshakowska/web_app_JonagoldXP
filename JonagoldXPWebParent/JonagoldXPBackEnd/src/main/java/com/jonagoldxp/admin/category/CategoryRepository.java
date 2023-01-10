83ad74a38b21e16fe4fe24591dadca640a4746f4
atorz <01169253@pw.edu.pl>
2023-01-10 14:29:35 +0100

    Added category repository representation class

--- JonagoldXPWebParent/JonagoldXPBackEnd/src/main/java/com/jonagoldxp/admin/category/CategoryRepository.java
+++ JonagoldXPWebParent/JonagoldXPBackEnd/src/main/java/com/jonagoldxp/admin/category/CategoryRepository.java
@@ -1,7 +1,9 @@
 package com.jonagoldxp.admin.category;
 
 import com.jonagoldxp.common.entity.Category;
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface CategoryRepository extends PagingAndSortingRepository<Category, Integer> {
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Integer> {
 }
