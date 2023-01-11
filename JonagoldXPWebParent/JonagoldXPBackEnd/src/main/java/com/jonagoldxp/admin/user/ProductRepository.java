f8d9df5033b6ac48e6d82b67bf58ff7bcb098875
atorz <01169253@pw.edu.pl>
2023-01-11 17:21:39 +0100

    Added product counting by id in database

--- JonagoldXPWebParent/JonagoldXPBackEnd/src/main/java/com/jonagoldxp/admin/user/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.jonagoldxp.admin.user;
-
-import com.jonagoldxp.common.entity.Product;
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface ProductRepository extends PagingAndSortingRepository<Product, Integer> {
-}
