0c3a1ac0691c20c27be3ad9350ccf34ad3e6a5d2
atorz <01169253@pw.edu.pl>
2023-01-11 22:04:22 +0100

    Removed arbitrary imports and removed image handling

--- JonagoldXPWebParent/JonagoldXPBackEnd/src/main/java/com/jonagoldxp/admin/category/CategoryController.java
+++ JonagoldXPWebParent/JonagoldXPBackEnd/src/main/java/com/jonagoldxp/admin/category/CategoryController.java
@@ -7,9 +7,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.util.StringUtils;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
@@ -39,7 +36,7 @@ public class CategoryController {
     }
 
     @PostMapping("/categories/save")
-    public String saveCategory(Category category, @RequestParam("fileImage")MultipartFile multipartFile, RedirectAttributes ra){
+    public String saveCategory(Category category, RedirectAttributes ra){
 
         service.save(category);
 
