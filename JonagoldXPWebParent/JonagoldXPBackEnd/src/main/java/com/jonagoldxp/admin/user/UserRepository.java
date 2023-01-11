7fb68065931fb55ba2e111a5b59673f87d97874e
mkozlo12 <01169195@pw.edu.pl>
2023-01-11 18:19:56 +0100

    Fixed bug with fetching data from queries. Implemented listByPage method in UserService so it will use paginating

--- JonagoldXPWebParent/JonagoldXPBackEnd/src/main/java/com/jonagoldxp/admin/user/UserRepository.java
+++ JonagoldXPWebParent/JonagoldXPBackEnd/src/main/java/com/jonagoldxp/admin/user/UserRepository.java
@@ -1,18 +1,25 @@
 package com.jonagoldxp.admin.user;
 
+import com.jonagoldxp.admin.paging.SearchRepository;
 import com.jonagoldxp.common.entity.User;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
 @Repository
-public interface UserRepository extends CrudRepository<User, Integer> {
+public interface UserRepository extends SearchRepository<User, Integer> {
     @Query("SELECT u FROM User u WHERE u.email = :email")
     public User getUserByEmail(@Param("email")String email);
+    public Long countById(Integer id);
 
-    @Query("SELECT u FROM User u WHERE concat(u.id, ' ', u.email, ' ', u.firstName, ' ', u.lastName) LIKE%?1%")
+    @Query(value = "SELECT u FROM User u WHERE concat(u.id, ' ', u.email, ' ', u.firstName, ' ', u.lastName) LIKE%?1%", nativeQuery = true)
     public Page<User> findAll(String keyword, Pageable pageable);
+
+    @Query("UPDATE User u SET u.enabled = ?2 WHERE u.id = ?1")
+    @Modifying
+    public void updateEnabledStatus(Integer id, boolean enabled);
 }
