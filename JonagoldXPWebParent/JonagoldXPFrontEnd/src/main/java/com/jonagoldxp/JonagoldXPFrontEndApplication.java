1d6c991a3a6e3f37685676ee84ae487043c847e1
mkozlo12 <01169195@pw.edu.pl>
2023-01-12 03:13:46 +0100

    Moved classes out of the site package

--- JonagoldXPWebParent/JonagoldXPFrontEnd/src/main/java/com/jonagoldxp/JonagoldXPFrontEndApplication.java
+++ JonagoldXPWebParent/JonagoldXPFrontEnd/src/main/java/com/jonagoldxp/JonagoldXPFrontEndApplication.java
@@ -1,4 +1,4 @@
-package com.jonagoldxp.site;
+package com.jonagoldxp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- JonagoldXPWebParent/JonagoldXPFrontEnd/src/main/java/com/jonagoldxp/MainController.java
+++ JonagoldXPWebParent/JonagoldXPFrontEnd/src/main/java/com/jonagoldxp/MainController.java
@@ -1,4 +1,4 @@
-package com.jonagoldxp.site;
+package com.jonagoldxp;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
