d4b7ca8b055c84376175776756199cf7b2026ce1
mkozlo12 <01169195@pw.edu.pl>
2023-01-13 23:58:03 +0100

    Added changes to the CustomerService and Repo so it will work with authorisation

--- JonagoldXPWebParent/JonagoldXPFrontEnd/src/main/java/com/jonagoldxp/customer/CustomerService.java
+++ JonagoldXPWebParent/JonagoldXPFrontEnd/src/main/java/com/jonagoldxp/customer/CustomerService.java
@@ -1,15 +1,34 @@
 package com.jonagoldxp.customer;
 
 import com.jonagoldxp.common.entity.Customer;
+import jakarta.annotation.Resource;
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
+
+import java.util.Date;
 
 @Service
+@Transactional
 public class CustomerService {
     @Autowired private CustomerRepository customerRepo;
 
     public boolean isEmailUnique(String email) {
         Customer customer = customerRepo.findByEmail(email);
         return customer == null;
+    }
+
+    public void registerCustomer(Customer customer) {
+        customer.setEnabled(false);
+        customer.setCreatedTime(new Date());
+        customer.setVerificationCode("random");
+
+        customerRepo.save(customer);
+
+    }
+
+    public Customer getCustomerByEmail(String email) {
+        return customerRepo.findByEmail(email);
     }
 }
