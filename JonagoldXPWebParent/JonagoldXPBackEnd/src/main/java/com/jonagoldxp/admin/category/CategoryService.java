7bb2fc7427e83197acaa1c1783765765e2c588fd
atorz <01169253@pw.edu.pl>
2023-01-19 14:22:35 +0100

    Added new paging and sorting methods

--- JonagoldXPWebParent/JonagoldXPBackEnd/src/main/java/com/jonagoldxp/admin/category/CategoryService.java
+++ JonagoldXPWebParent/JonagoldXPBackEnd/src/main/java/com/jonagoldxp/admin/category/CategoryService.java
@@ -2,16 +2,91 @@ package com.jonagoldxp.admin.category;
 
 import com.jonagoldxp.common.entity.Category;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.NoSuchElementException;
-import java.util.Set;
+import java.util.*;
 
 @Service
 public class CategoryService {
+    public static final int ROOT_CATEGORIES_PER_PAGE = 4;
     @Autowired CategoryRepository repo;
+
+    public List<Category> listByPage(CategoryPageInfo pageInfo, int pageNum, String sortDir,
+                                     String keyword) {
+        Sort sort = Sort.by("name");
+
+        if (sortDir.equals("asc")) {
+            sort = sort.ascending();
+        } else if (sortDir.equals("desc")) {
+            sort = sort.descending();
+        }
+
+        Pageable pageable = PageRequest.of(pageNum - 1, ROOT_CATEGORIES_PER_PAGE, sort);
+
+        Page<Category> pageCategories = null;
+
+        if (keyword != null && !keyword.isEmpty()) {
+            pageCategories = repo.search(keyword, pageable);
+        } else {
+            pageCategories = repo.findRootCategories(pageable);
+        }
+
+        List<Category> rootCategories = pageCategories.getContent();
+
+        pageInfo.setTotalElements(pageCategories.getTotalElements());
+        pageInfo.setTotalPages(pageCategories.getTotalPages());
+
+        if (keyword != null && !keyword.isEmpty()) {
+            List<Category> searchResult = pageCategories.getContent();
+            for (Category category : searchResult) {
+                category.setHasChildren(category.getChildren().size() > 0);
+            }
+
+            return searchResult;
+
+        } else {
+            return listHierarchicalCategories(rootCategories, sortDir);
+        }
+    }
+
+    private List<Category> listHierarchicalCategories(List<Category> rootCategories, String sortDir) {
+        List<Category> hierarchicalCategories = new ArrayList<>();
+
+        for (Category rootCategory : rootCategories) {
+            hierarchicalCategories.add(Category.copyFull(rootCategory));
+
+            Set<Category> children = sortSubCategories(rootCategory.getChildren(), sortDir);
+
+            for (Category subCategory : children) {
+                String name = "--" + subCategory.getName();
+                hierarchicalCategories.add(Category.copyFull(subCategory, name));
+
+                getSubCategories(hierarchicalCategories, subCategory, 1, sortDir);
+            }
+        }
+
+        return hierarchicalCategories;
+    }
+
+    private void getSubCategories(List<Category> categories, Category parent, int subLevel, String sortDir){
+        int nextSubLevel = subLevel + 1;
+        Set<Category> children = sortSubCategories(parent.getChildren(), sortDir);
+
+        for (Category subCategory : children){
+            StringBuilder pronoun = new StringBuilder();
+            for (int i = 0; i < nextSubLevel; i++){
+                pronoun.append("--");
+            }
+            categories.add(Category.copyFull(subCategory, pronoun + subCategory.getName()));
+
+            getSubCategories(categories, subCategory, nextSubLevel, sortDir);
+        }
+    }
+
 
     public List<Category> listAll(){
         return repo.findAll();
@@ -82,6 +157,28 @@ public class CategoryService {
 
         return "OK";
     }
+
+    private SortedSet<Category> sortSubCategories(Set<Category> children) {
+        return sortSubCategories(children, "asc");
+    }
+
+    private SortedSet<Category> sortSubCategories(Set<Category> children, String sortDir) {
+        SortedSet<Category> sortedChildren = new TreeSet<>(new Comparator<Category>() {
+            @Override
+            public int compare(Category cat1, Category cat2) {
+                if (sortDir.equals("asc")) {
+                    return cat1.getName().compareTo(cat2.getName());
+                } else {
+                    return cat2.getName().compareTo(cat1.getName());
+                }
+            }
+        });
+
+        sortedChildren.addAll(children);
+
+        return sortedChildren;
+    }
+
     public void updateCategoryEnabledStatus(Integer id, boolean enabled) {
         repo.updateEnabledStatus(id, enabled);
     }
